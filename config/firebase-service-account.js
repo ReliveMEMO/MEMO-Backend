ceefaab8c037adbe941ca3229d6b6b10e418e54e
SachinKule17 <sachin.20230582@iit.ac.lk>
2025-02-07 22:00:58 +0530

    Update firebase-service-account.js

--- config/firebase-service-account.js
+++ config/firebase-service-account.js
@@ -4,4 +4,4 @@ admin.initializeApp({
     credential: admin.credential.cert(require('./memo-ab7e1-firebase-adminsdk-8vt29-ceac478057.json')),
 });
 
-module.exports = admin;
+module.exports = admin; // Export the Firebase Admin module
