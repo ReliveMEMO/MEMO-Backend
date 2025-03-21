b8339cf27e07ef2c622250491e11945ea6e631f4
Malsha-J <malshapjayasinghe2002@gmail.com>
2025-03-21 11:39:13 +0530

    2025/03/21

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -53,39 +53,4 @@ async function sendGroupMessage(req, res) {
     });
 }
 
-// async function sendMessage(req, res) {
-//     const { senderId, receiverId, message } = req.body;
-
-//     // Debugging
-//     console.log("Sender ID:", senderId);
-//     console.log("Receiver ID:", receiverId);
-//     console.log("Message to encrypt:", message);
-
-//     if (!message || typeof message !== 'string') {
-//         return res.status(400).json({ error: "Invalid message. It must be a non-empty string." });
-//     }
-
-//     const timestamp = new Date().toISOString();
-//     const encryptedMessage = encrypt(message);
-
-//     const { chatId, error: chatError } = await findOrCreateChat(senderId, receiverId);
-//     if (chatError) {
-//         console.error("Chat creation error:", chatError);
-//         return res.status(500).json({ error: chatError.message });
-//     }
-
-//     const messageObject = { [timestamp]: encryptedMessage };
-//     console.log("Message Object to append:", messageObject);
-
-//     const { data, error } = await appendMessage(chatId, messageObject);
-
-//     if (error) {
-//         console.error("Error appending message:", error);
-//         return res.status(500).json({ error: error.message });
-//     }
-
-//     res.status(200).json({ data });
-// }
-
-
 module.exports = { sendMessage, sendGroupMessage };
